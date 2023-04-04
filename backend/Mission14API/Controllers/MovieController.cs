d8c9174282c18401b179b18f52605a9c9a2af8ed
laladean <111604211+laladean@users.noreply.github.com>
2023-04-04 00:56:45 -0600

    Add files via upload

---
+++ backend/Mission14API/Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission14API.Data;
+using System.Runtime.CompilerServices;
+
+namespace Mission14API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        private MovieDbContext context;
+        public MovieController(MovieDbContext temp) {
+            context = temp;
+        }
+        public IEnumerable<MovieCollection> Get()
+        {
+            //Only display edited movies, ordered alphabetically by title
+            var x = context.Movies.ToArray()
+                .Where(m => m.Edited == "Yes")
+                .OrderBy(m => m.Title);
+            return x;
+        }
+
+    }
+}
---
+++ frontend/src/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <headerclassName="row">
+            <div className="col-4">
+                <img src=
+        </header>
+    );
+}
+
+export default Header;
